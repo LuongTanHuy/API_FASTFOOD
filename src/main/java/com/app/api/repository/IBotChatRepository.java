56ecc06a3532d75fd7202069444534379e08faee
LuongTanHuy <132861859+LuongTanHuy@users.noreply.github.com>
2025-04-21 01:15:03 +0700

    Replace old code with new version

--- src/main/java/com/app/api/repository/IBotChatRepository.java
+++
@@ -1,8 +0,0 @@
-package com.app.api.repository;
-
-import com.app.api.model.BotChat;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IBotChatRepository extends JpaRepository<BotChat,Integer> {
-
-}
